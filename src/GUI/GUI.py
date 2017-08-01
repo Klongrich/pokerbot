35fb9b8818ea5568d49a0e0ca526d6abbac4ae2e
Klongrich <longrichk@gmail.com>
2017-08-01 12:15:59 -0400

    Adding cards
    Working on spacing

--- src/GUI/GUI.py
+++ src/GUI/GUI.py
@@ -4,14 +4,21 @@ class Table:
 
     def __init__(self, master):
         frame = Frame(master)
-
         frame.pack()
 
         ''' Here is where the table cards should be shown'''
         self.cardOne = Label(frame, image=CardOne)
         self.cardTwo = Label(frame, image=CardTwo)
+        self.cardThree = Label(frame, image=CardThree)
+        self.cardFour = Label(frame, image=CardFour)
+        self.cardFive = Label(frame, image=CardFive)
+
         self.cardOne.grid(row=0)
         self.cardTwo.grid(row=0, column=1)
+        self.cardThree.grid(row=0, column=2)
+        self.cardFour.grid(row=0, column=3)
+        self.cardFive.grid(row=0, column=4)
+
 
         ''' All the buttons for doing stuff'''
         self.chipsLabel = Label(frame, text="Amount Of Chips: 714,000")
@@ -32,9 +39,17 @@ class Table:
         self.addButton = Button(frame, text="Raise", command=self.Raise)
         self.addButton.grid(row=2, column=4)
 
-        ''' Here is where the pic of the 2 cards you have should go '''
-        
+        ''' Where your cards are shown '''
 
+        self.player_cardone = Label(frame, image=player_CardOne)
+        self.player_cardtwo = Label(frame, image=player_CardTwo)
+
+        self.player_cardone.grid(row=3, column=0)
+        self.player_cardtwo.grid(row=3, column=1)
+
+
+
+    ''' Feel like this will be pretty simple '''
     def bet(self):
         print("hello");
 
@@ -51,10 +66,17 @@ class Table:
         print("hello");
 
 root = Tk()
+root.title("Poker")
 
 CardOne = PhotoImage(file="card_pics/aceofspades.png")
-CardTwo = PhotoImage(file="card_pics/aceofdimandos.png")
-root.minsize(height=417, width=437)
-root.maxsize(height=417, width=437)
+CardTwo = PhotoImage(file="card_pics/2ofspades.png")
+CardThree = PhotoImage(file="card_pics/threeofspades.png")
+CardFour = PhotoImage(file="card_pics/fourofspades.png")
+CardFive = PhotoImage(file="card_pics/aceofspades.png")
+
+player_CardOne = PhotoImage(file="card_pics/twoofhearts.png")
+player_CardTwo = PhotoImage(file="card_pics/threeofhearts.png")
+root.minsize(height=417, width=467)
+root.maxsize(height=417, width=467)
 k = Table(root)
 root.mainloop()

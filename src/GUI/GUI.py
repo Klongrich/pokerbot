c42c0c27ce4d8e70ca96f7c9d5a9ab8fac801ce1
Klongrich <longrichk@gmail.com>
2017-08-01 12:55:33 -0400

    Cards in better posistion.
    Had to switch from .pack() to .grid() layout. No clue how it worked so I
    just messed around with it for a little

--- src/GUI/GUI.py
+++ src/GUI/GUI.py
@@ -7,45 +7,46 @@ class Table:
         frame.pack()
 
         ''' Here is where the table cards should be shown'''
+
         self.cardOne = Label(frame, image=CardOne)
         self.cardTwo = Label(frame, image=CardTwo)
         self.cardThree = Label(frame, image=CardThree)
         self.cardFour = Label(frame, image=CardFour)
         self.cardFive = Label(frame, image=CardFive)
 
-        self.cardOne.grid(row=0)
-        self.cardTwo.grid(row=0, column=1)
-        self.cardThree.grid(row=0, column=2)
-        self.cardFour.grid(row=0, column=3)
-        self.cardFive.grid(row=0, column=4)
+        self.cardOne.grid(row=1, column=0, sticky=E)
+        self.cardTwo.grid(row=1, column=1)
+        self.cardThree.grid(row=1, column=2)
+        self.cardFour.grid(row=1, column=3)
+        self.cardFive.grid(row=1, column=4)
 
 
         ''' All the buttons for doing stuff'''
-        self.chipsLabel = Label(frame, text="Amount Of Chips: 714,000")
-        self.chipsLabel.grid(row=1, column=1)
+        #self.chipsLabel = Label(frame, text="Amount Of Chips: 714,000")
+        #self.chipsLabel.grid(row=2, column=0, pady=40, padx=0)
 
-        self.addButton = Button(frame, text="Bet", width=14, command=self.bet)
-        self.addButton.grid(row=2, column=0)
+        self.addButton = Button(frame, text="Bet", width=10, command=self.bet)
+        self.addButton.grid(row=3, column=0, pady=80)
 
-        self.addButton = Button(frame, text="check", command=self.check)
-        self.addButton.grid(row=2, column=1)
+        self.addButton = Button(frame, text="check", width=10, command=self.check)
+        self.addButton.grid(row=3, column=1)
 
-        self.addButton = Button(frame, text="Fold", command=self.fold)
-        self.addButton.grid(row=2, column=2)
+        self.addButton = Button(frame, text="Fold", width=10, command=self.fold)
+        self.addButton.grid(row=3, column=2)
 
-        self.addButton = Button(frame, text="Call", command=self.call)
-        self.addButton.grid(row=2, column=3)
+        self.addButton = Button(frame, text="Call", width=10, command=self.call)
+        self.addButton.grid(row=3, column=3)
 
-        self.addButton = Button(frame, text="Raise", command=self.Raise)
-        self.addButton.grid(row=2, column=4)
+        self.addButton = Button(frame, text="Raise", width=10, command=self.Raise)
+        self.addButton.grid(row=3, column=4)
 
         ''' Where your cards are shown '''
 
         self.player_cardone = Label(frame, image=player_CardOne)
         self.player_cardtwo = Label(frame, image=player_CardTwo)
 
-        self.player_cardone.grid(row=3, column=0)
-        self.player_cardtwo.grid(row=3, column=1)
+        self.player_cardone.grid(row=4, column=0)
+        self.player_cardtwo.grid(row=4, column=1)
 
 
 
